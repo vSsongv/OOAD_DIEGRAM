4929d514cc8adb2a81ecebbd84b52ee0be8d16a9
suhyeonkim <yunatlsghk6@ajou.ac.kr>
2020-12-04 23:08:03 +0900

    First Draft

---
+++ src/DIEGRAM/Diegram.java
@@ -0,0 +1,5 @@
+package DIEGRAM;
+
+public class Diegram {
+
+}
---
+++ src/DIEGRAM/UI_Layer.java
@@ -0,0 +1,10 @@
+package DIEGRAM;
+
+public class UI_Layer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
